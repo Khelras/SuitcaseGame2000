cc95f400517d548e9525f0bcf8a315a215bf708d
Khelras <anjello.30bohol@gmail.com>
2026-06-18 10:55:41 +1200

    Stages Cleanup

--- scripts/StageSelect.cs
+++ scripts/StageSelect.cs
@@ -5,31 +5,31 @@ public partial class StageSelect : Control //We use Control instead of Node2D be
 {
     private void OnStage1Pressed()
     {
-        GetTree().ChangeSceneToFile("res://scenes/Stage1Levels.tscn"); //Change the scene to Stage1 when the button is pressed
+        GetTree().ChangeSceneToFile("res://scenes/Stages/Stage1Levels.tscn"); //Change the scene to Stage1 when the button is pressed
     }
 
     private void OnStage2Pressed()
     {
-        GetTree().ChangeSceneToFile("res://scenes/Stage2Levels.tscn"); //Change the scene to Stage2 when the button is pressed
+        GetTree().ChangeSceneToFile("res://scenes/Stages/Stage2Levels.tscn"); //Change the scene to Stage2 when the button is pressed
     }
 
     private void OnStage1Level1Pressed()
     {
-        GetTree().ChangeSceneToFile("res://scenes/Stage1Levels/Stage1Level1.tscn"); //Change the scene to Stage1Level1 when the button is pressed
+        GetTree().ChangeSceneToFile("res://scenes/Stages/Stage1Levels/Stage1Level1.tscn"); //Change the scene to Stage1Level1 when the button is pressed
     }
 
     private void OnStage1Level2Pressed()
     {
-        GetTree().ChangeSceneToFile("res://scenes/Stage1Levels/Stage1Level2.tscn"); //Change the scene to Stage1Level2 when the button is pressed
+        GetTree().ChangeSceneToFile("res://scenes/Stages/Stage1Levels/Stage1Level2.tscn"); //Change the scene to Stage1Level2 when the button is pressed
     }
 
     private void OnStage2Level1Pressed()
     {
-        GetTree().ChangeSceneToFile("res://scenes/Stage2Levels/Stage2Level1.tscn"); //Change the scene to Stage2Level1 when the button is pressed
+        GetTree().ChangeSceneToFile("res://scenes/Stages/Stage2Levels/Stage2Level1.tscn"); //Change the scene to Stage2Level1 when the button is pressed
     }
     private void OnStage2Level2Pressed()
     {
-        GetTree().ChangeSceneToFile("res://scenes/Stage2Levels/Stage2Level2.tscn"); //Change the scene to Stage2Level2 when the button is pressed
+        GetTree().ChangeSceneToFile("res://scenes/Stages/Stage2Levels/Stage2Level2.tscn"); //Change the scene to Stage2Level2 when the button is pressed
     }
 
     private void OnBackPressed()
