6fbfae706e5efb6ca2e0b9035701f2da99bb808d
Khelras <anjello.30bohol@gmail.com>
2026-06-18 12:22:54 +1200

    Relative Transform Properties for all Items

--- scripts/ItemManeger.cs
+++ scripts/ItemManeger.cs
@@ -6,32 +6,21 @@ using static Items;
 
 public partial class ItemManeger : Node
 {
-    // Public Variables set Externally in the Editor
-    [Export] public PackedScene itemsScene;
-
     private Items items;
-    private Button swapbutton;
-    private DragAndDrop dragAndDrop;
-    private Area2D area;
-
     private int currentItem;
-    private bool itemSpawned = false;
+    private Button swapbutton;
+    private Area2D area;
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        // Check if PackedScene Reference is Set
-        if (this.itemsScene == null)
-        {
-            // DEBUG: If the TileMapLayer reference is not set, print a warning to the console
-            GD.PushWarning("itemsScene reference is not set. Please assign it in the inspector.");
-        }
-        else
-        {
-            // DEBUG: Print the name of the TileMapLayer to confirm the reference is set correctly
-            GD.Print("itemsScene reference is set successfully.");
-        }
-       
+        // Items
+        items = GD.Load<PackedScene>("res://scenes/Items.tscn").Instantiate<Items>();
+        AddChild(items);
+        currentItem = 0; // First Item
+        items.SetItem(0);
+        
+        // Item Swapping
         area = GetNode<Area2D>("Area2D");
         swapbutton = GetNode<Button>("Button");
         swapbutton.Pressed += OnSwapButtonPressed;
@@ -40,39 +29,20 @@ public partial class ItemManeger : Node
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-    {   // only spawn once
-        if (!itemSpawned) {
-
-            SpawnNode(currentItem); // Spawns the sprite
-            itemSpawned = true;
-        } 
-        
+    {
     }
+
     private void OnAreaExited(Area2D area) {
         GD.Print("Exit");
         items.RemoveItem(currentItem);
     }
+
     private void OnSwapButtonPressed() {
-      if (items == null || items.orderSprites.Count <= 0) return;
+        // Ensure Items Exists
+        if (items == null || items.orderSprites.Count <= 0) return;
+
+        // Swap to the Next Item
         currentItem = (currentItem + 1) % items.orderSprites.Count;
-        
         items.SetItem(currentItem);
-
     }
-	private void SpawnNode(int index) {
-       if (items != null) return; // dont spawn if item does't exist
-        // create a copy of the scene
-        items = itemsScene.Instantiate<Items>();
-
-        AddChild(items);
-
-        items.SetItem(index);
-    }
-    private void SetItemType(int index) {
-        if (IsInstanceValid(items))
-            items.SetItem(index);
-    }
-
-  
-	
 }
