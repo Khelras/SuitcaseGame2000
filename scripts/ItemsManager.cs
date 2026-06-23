e30c7f17f74077eafd9ce3e3431b4b1da69505db
Zzzenki <Robert.Weber@mds.ac.nz>
2026-06-23 16:11:32 +1200

    ItemManager
    Items that are sentimental now show when done packing with their title and discription fadding in and out
    
    only shows 3 items, i feel like more than that would be annoying

--- scripts/ItemsManager.cs
+++ scripts/ItemsManager.cs
@@ -1,10 +1,16 @@
 using Godot;
+using System.Linq;
 using System;
 using System.Collections.Generic;
 
 public partial class ItemsManager : Node
 {
-	public List<Item> GetAllItems()
+    [Export] public Control LeftBehindPanel;
+    [Export] public TextureRect LeftBehindImage;
+    [Export] public Label LeftBehindText;
+    [Export] public Label LeftBehindDescription;
+
+    public List<Item> GetAllItems()
 	{
 		List<Item> items = new();
 
@@ -30,7 +36,7 @@ public partial class ItemsManager : Node
 			}
 		}
 		return leftBehindItems;
-    
+
 	}
 
 	// Called when the node enters the scene tree for the first time.
@@ -42,4 +48,62 @@ public partial class ItemsManager : Node
 	public override void _Process(double delta)
 	{
 	}
+
+	private async void OnButtonPressed()
+	{
+        List<Item> leftBehindItems = GetLeftBehindItems();
+
+        List<Item> emotionalItems = new();
+
+        foreach (Item item in leftBehindItems)
+        {
+            if (item.Catagory == ItemCatagory.Sentimental ||
+                item.Catagory == ItemCatagory.CulturalIdentity)
+            {
+                emotionalItems.Add(item);
+            }
+        }
+
+        Random random = new Random();
+
+        emotionalItems = emotionalItems.OrderBy(x => random.Next()).Take(3).ToList();
+
+        LeftBehindPanel.Visible = true;
+        LeftBehindPanel.Modulate = new Color(1, 1, 1, 0);
+
+        LeftBehindImage.Modulate = new Color(1, 1, 1, 0);
+        LeftBehindText.Modulate = new Color(1, 1, 1, 0);
+        LeftBehindDescription.Modulate = new Color(1, 1, 1, 0);
+
+        // Fade whole screen to black
+        Tween screenTween = CreateTween();
+        screenTween.TweenProperty(LeftBehindPanel, "modulate:a", 1.0, 1.0);
+        await ToSignal(screenTween, "finished");
+
+        foreach (Item item in emotionalItems)
+        {
+            LeftBehindImage.Texture = item.Texture;
+            LeftBehindText.Text = "Left behind: " + item.ItemName;
+            LeftBehindDescription.Text = item.ItemDescription;
+
+            // Fade item/text in
+            Tween fadeIn = CreateTween();
+            fadeIn.TweenProperty(LeftBehindImage, "modulate:a", 1.0, 0.6);
+            fadeIn.Parallel().TweenProperty(LeftBehindText, "modulate:a", 1.0, 0.6);
+            fadeIn.Parallel().TweenProperty(LeftBehindDescription, "modulate:a", 1.0, 0.6);
+            await ToSignal(fadeIn, "finished");
+
+            await ToSignal(GetTree().CreateTimer(2.0), "timeout");
+
+            // Fade item/text out
+            Tween fadeOut = CreateTween();
+            fadeOut.TweenProperty(LeftBehindImage, "modulate:a", 0.0, 0.6);
+            fadeOut.Parallel().TweenProperty(LeftBehindText, "modulate:a", 0.0, 0.6);
+            fadeOut.Parallel().TweenProperty(LeftBehindDescription, "modulate:a", 0.0, 0.6);
+            await ToSignal(fadeOut, "finished");
+        }
+
+        LeftBehindPanel.Visible = false;
+
+    }
 }
