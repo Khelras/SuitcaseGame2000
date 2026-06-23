c2fdb4703eb1f511f7759ab94623ce4446198c79
Catnip_userXd <bidabida475@gmail.com>
2026-06-23 20:22:49 +1200

    SFX, edited script, Item discription

--- scripts/ItemHoverUI.cs
+++ scripts/ItemHoverUI.cs
@@ -6,20 +6,30 @@ public partial class ItemHoverUI : Control
 	[Export] public Label NameLabel;
 	[Export] public Label CatagoryLabel;
 	[Export] public Label DescriptionLabel;
+    private AudioStreamPlayer2D sfxShowItem;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
 		Visible = false;
-	}
+        sfxShowItem = GetNode<AudioStreamPlayer2D>("ShowItem");
+		FontFile customFont = GD.Load<FontFile>("res://Fonts/KiriFont.otf");
+		// Creating a new labelSetting instance and apply the font
+        NameLabel.LabelSettings = new LabelSettings();
+        CatagoryLabel.LabelSettings = new LabelSettings();
+        DescriptionLabel.LabelSettings = new LabelSettings();
+		NameLabel.LabelSettings.Font = customFont;
+        CatagoryLabel.LabelSettings.Font = customFont;
+        DescriptionLabel.LabelSettings.Font = customFont;
+    }
 
 	public void ShowItem(Item item)
 	{
 		NameLabel.Text = item.ItemName;
 		CatagoryLabel.Text = item.Catagory.ToString();
 		DescriptionLabel.Text = item.ItemDescription;
-
-		Visible = true;
+		sfxShowItem.Play();
+        Visible = true;
 	}
 
 	public void HideItem(Item item)
